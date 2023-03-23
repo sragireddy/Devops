d9fc5fd7f98e2650409213543f30f0c6839b7697
sragireddy <73263601+sragireddy@users.noreply.github.com>
2023-03-23 19:33:42 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,19 +1,13 @@
-import imaplib
+import win32com.client
 
-# account credentials
-username = "your_email_address"
-password = "your_password"
+# connect to Outlook
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+inbox = outlook.GetDefaultFolder(6)  # 6 refers to the inbox folder
 
-# create an IMAP4 class with SSL 
-imap = imaplib.IMAP4_SSL("imap-mail.outlook.com")
+# get the latest email
+messages = inbox.Items
+message = messages.GetLast()
 
-# attempt to authenticate
-try:
-    imap.login(username, password)
-    print("Login successful!")
-except imaplib.IMAP4.error:
-    print("Login failed.")
-
-# close the mailbox and logout
-imap.close()
-imap.logout()
+# print the subject and sender
+print("Subject:", message.Subject)
+print("Sender:", message.SenderEmailAddress)
